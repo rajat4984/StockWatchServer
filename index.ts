eac2c160dc2b513d9adee09d4ad6b5f2ef5370b8
rajat4984 <rs20021023@gmail.com>
2024-12-09 13:09:28 +0530

    added typescript

---
+++ index.ts
@@ -0,0 +1,13 @@
+// src/index.js
+import express, { Express, Request, Response } from "express";
+
+const app = express();
+const port = 4000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
